edaaeae6d5e51313788f64979c6751c177030c60
pau <paugitonga6@gmail.com>
2022-07-06 11:54:30 +0300

    done

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Code your solution in this file!
-const companyName = 'Scuber';
-let mostProfitableNeighborhood='Chelsea';
+const companyName="Scuber"
+let mostProfitableNeighborhood="Chelsea"
 let companyCeo="Susan Smith"
